5c4e561e948c179677f9c4a9a7ce35261a3ea1a1
Timofey Cvetkov <timofey.cvetkov@protonmail.com>
2020-07-20 15:33:53 +0100

    Updates

---
+++ precache-manifest.31a60c5b0222f2266ee0a7452c1ea62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac75b6928fb8646e54cbb9600fed547",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "dcd46e3f6a5cd3c76cb3",
+    "url": "/random-quote-generator/static/css/main.09557cc1.chunk.css"
+  },
+  {
+    "revision": "2384b7aafcdc87349cdc",
+    "url": "/random-quote-generator/static/js/2.8a21fb19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.8a21fb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd46e3f6a5cd3c76cb3",
+    "url": "/random-quote-generator/static/js/main.8010b702.chunk.js"
+  },
+  {
+    "revision": "493d3c787c14b3f3ee75",
+    "url": "/random-quote-generator/static/js/runtime-main.be855b98.js"
+  }
+]);
